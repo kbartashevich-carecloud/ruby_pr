ada96924ad1f061a6356eb70a05568efd978cf95
Kiryl <kbartashevich@carecloud.com>
2020-02-28 16:09:41 -0500

    Struct classes

--- OOP.rb
+++ OOP.rb
@@ -1,23 +1,63 @@
-class Gear
+# class Gear
+#   private
+#   attr_reader :chainring, :cog
+#
+#   public
+#   def initialize(chainring, cog)
+#     @chainring = chainring
+#     @cog = cog
+#   end
+#
+#   def ratio
+#     chainring / cog.to_f
+#   end
+#
+#   def gear_inches
+#     ratio * (rim + (tire * 2))
+#   end
+#
+# end
+#
+# class Blinkered
+#   def cog(gear)
+#     gear.cog
+#   end
+# end
+#
+# puts Blinkered.new.cog(Gear.new(54, 11))
+#
 
-  attr_reader :chainring, :cog, :rim, :tire
-
-  def initialize(chainring, cog, rim, tire)
-    @chainring = chainring
-    @cog = cog
-    @rim = rim
-    @tire = tire
+class ObscuringReferences
+  attr_reader :data
+  def initialize(data)
+    @data = data
   end
 
-  def ratio
-    chainring / cog.to_f
+  def diameters
+    # 0 is rim, 1 is tire
+    data.collect do |cell|
+      cell[0] + (cell[1] * 2)
+    end
   end
-
-  def gear_inches
-    ratio * (rim + (tire * 2))
-  end
-
 end
 
-puts Gear.new(52, 11, 26, 1.5).gear_inches
-puts Gear.new(52, 11, 24, 1.25).gear_inches
+class RevealingReferences
+  attr_reader :wheels
+  def initialize(data)
+    @wheels = wheelify(data)
+  end
+
+  def diameters
+    wheels.collect do |wheel|
+      wheel.rim + (wheel.tire * 2)
+    end
+  end
+
+  Wheel = Struct.new(:rim, :tire)
+
+  def wheelify(data)
+    data.collect do |cell|
+      Wheel.new(cell[0], cell[1])
+    end
+  end
+end
