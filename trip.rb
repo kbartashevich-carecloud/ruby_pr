f36adf8d14034aa6794721d8faea0d241cdcb16b
Kiryl <kbartashevich@carecloud.com>
2020-02-29 13:38:45 -0500

    common interface

--- trip.rb
+++ trip.rb
@@ -4,16 +4,7 @@ class Trip
 
   def prepare(preparers)
     preparers.each do |preparer|
-      case preparer
-
-      when Mechanic
-        preparer.prepare_bicycles(bicycles)
-      when TripCoordinator
-        preparer.buy_food(customers)
-      when Driver
-        prepare.gas_up(vehicle)
-        preparer.fill_water_tank(vehicle)
-      end
+      preparer.prepare_trip(self)
     end
   end
 
@@ -21,30 +12,26 @@ end
 
 class Mechanic
 
-  def prepare_bicycles(bicycles)
-    bicycles.each {|bicycle| prepare_bicycle(bicycle)}
-  end
-
-  def prepare_bicycle(bicycle)
-    # ....
+  def prepare_trip(trip)
+    trip.bicycles.each do |bicycle|
+      prepare_bicycle(bicycle)
+    end
   end
 
 end
 
 class TripCoordinator
-  def buy_food(customers)
-    # ...
+  def prepare_trip(trip)
+    buy_food(trip.customers)
   end
 end
 
 class Driver
 
-  def gas_up(vehicle)
-    # ...
-  end
-
-  def fill_water_tank(vihicle)
-    # ...
+  def prepare_trip(trip)
+    vehicle = trip.vehicle
+    gas_up(vehicle)
+    fill_water_tank(vehicle)
   end
 
 end
