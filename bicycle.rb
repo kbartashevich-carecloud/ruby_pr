ee65e5c69a0d025cee551d5250161e8b874258c7
Kiryl <kbartashevich@carecloud.com>
2020-02-29 19:11:44 -0500

    Inheritance added

---
+++ bicycle.rb
@@ -0,0 +1,150 @@
+# class Bicycle
+#
+#   attr_reader :style, :size,
+#               :tape_color,
+#               :front_shock, :rear_shock
+#
+#
+#
+#   def initialize(**opts)
+#     @style = opts[:style]
+#     @size = opts[:size]
+#     @tape_color = opts[:tape_color]
+#     @front_shock = opts[:front_shock]
+#     @rear_shock = opts[:rear_shock]
+#   end
+#
+#   def spares
+#
+#     if style == :road
+#
+#       { chain: "li-speed",
+#         tire_size: "23",
+#         tape_color: tape_color }
+#     else
+#
+#       { chain: "li-speed",
+#         tire_size: "2.1",
+#         front_shock: front_shock }
+#       end
+#   end
+#
+# end
+#
+#
+# bike = Bicycle.new(
+#     style: :mountain,
+#     size: 'S',
+#     front_shock: 'Manitou',
+#     rear_shock: 'Fox'
+# )
+#
+#
+# puts bike.spares
+#
+#
+#
+# class MountainBike < Bicycle
+#
+#   attr_reader :front_shock, :rear_shock
+#
+#   def initialize(**opts)
+#
+#     @front_shock = opts[:front_shock]
+#     @rear_shock = opts[:rear_shock]
+#
+#     super
+#
+#   end
+#
+#   def spares
+#
+#     super.merge(front_shock: front_shock)
+#
+#   end
+#
+# end
+#
+
+
+class Bicycle
+
+  attr_reader :size, :chain, :tire_size
+
+  def initialize(**opts)
+    @size = opts[:size]
+    @chain = opts[:chain] || default_chain
+    @tire_size = opts[:tire_size] || default_tire_size
+  end
+
+  def default_chain
+
+    "l1-speed"
+
+  end
+
+end
+
+
+class RoadBike < Bicycle
+
+  attr_reader :tape_color
+
+  def initialize(**opts)
+
+    @tape_color = opts[:tape_color]
+    super
+
+  end
+
+  def spares
+
+    { chain: '11-speed',
+      tire_size: '23',
+      tape_color: tape_color }
+
+  end
+
+  def default_tire_size
+
+    "23"
+
+  end
+
+end
+
+class MountainBike < Bicycle
+
+  attr_reader :front_shock
+
+  def spares
+
+    super.merge(front_shock: front_shock)
+
+  end
+
+  def default_tire_size
+
+    "2.1"
+
+  end
+
+end
+
+road_bike = RoadBike.new(
+    size: 'M',
+    tape_color: 'red'
+)
+
+puts road_bike.tire_size
+puts road_bike.chain
+
+mountain_bike = MountainBike.new(
+    size: "S",
+    front_shock: "Manitou",
+    rear_shock: "Fox"
+)
+
+puts mountain_bike.tire_size
+puts mountain_bike.chain
+
